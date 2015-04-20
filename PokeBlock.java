bc0f53715964f1da4624ce67b571327fe7cfa872
Gigarion <cdgum@princeton.edu>
2015-04-19 23:29:01 -0400

    smalls

---
+++ PokeBlock.java
@@ -0,0 +1,14 @@
+public class PokeBlock {
+	public static final String image;
+	public static final double LENGTH;
+	public static final double HEIGHT;
+
+	public PokeBlock (Pokemon poke) {
+		image = poke.getName() + ".png";
+
+	}
+
+	public void draw() {
+
+	}
+}
